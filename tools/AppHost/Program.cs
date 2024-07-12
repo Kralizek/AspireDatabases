1c0f93da7212f4a32602f9d9d9ff51f889f9dfdc
Renato Golia <renato.golia@tobiidynavox.com>
2024-07-12 09:24:22 +0200

    Added databases to AppHost

--- tools/AppHost/Program.cs
+++ tools/AppHost/Program.cs
@@ -1,3 +1,24 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var password = builder.AddParameter("Password");
+
+var cosmos = builder.AddAzureCosmosDB("cosmos")
+                    .RunAsEmulator()
+                    .AddDatabase("cosmosdb");
+
+builder.AddProject<Projects.CosmosDbWeb>("cosmosWeb")
+       .WithReference(cosmos);
+
+var mssql = builder.AddSqlServer("mssql", password)
+                   .AddDatabase("mssqldb");
+
+builder.AddProject<Projects.MsSqlWeb>("mssqlWeb")
+       .WithReference(mssql);
+
+var npgsql = builder.AddPostgres("pg", password)
+                    .AddDatabase("pgDb");
+
+builder.AddProject<Projects.PostgreSqlWeb>("pgWeb")
+       .WithReference(npgsql);
+
 builder.Build().Run();
